b43a327e6c9fd22c73e7f71dec8716a136bbdd0f
anonyfriday <duykimvu75@gmail.com>
2025-10-04 03:20:59 +0700

    add(add-more-number): add morenumber to demonstrate the stream did not close until sending completeasync

---
+++ GrpcClientClientStreaming/Program.cs
@@ -0,0 +1,45 @@
+﻿
+var channel = Grpc.Net.Client.GrpcChannel.ForAddress("http://localhost:5286");
+var client = new Calculator.CalculatorClient(channel);
+
+bool continueAdding = true;
+
+while (continueAdding)
+{
+    using (var call = client.AddStream())
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            // return a task that only completes when the data has been writtten to
+            // the local buffer, and the stream is ready for next chunk
+            // so that;s why must use await here
+            await call.RequestStream.WriteAsync(new AddRequest { Number = (uint)i });
+            Console.WriteLine($"Sent number: {i}");
+        }
+
+        // Testing if the stream is not close but can send more numbers
+        Console.WriteLine("Do you want to sent an individual number? (0: No, 1: Yes)");
+        var moreNumber = Console.ReadLine();
+        if (moreNumber == "1")
+        {
+            Console.WriteLine("Enter a number to send:");
+            int.TryParse(Console.ReadLine(), out int newNumber);
+            await call.RequestStream.WriteAsync(new AddRequest { Number = (uint)newNumber });
+            Console.WriteLine($"Sent number: {newNumber}");
+        }
+
+        // Complete the request stream to indicate that no more messages will be sent
+        // Once u completed, the stream cannot be reuse
+        await call.RequestStream.CompleteAsync();
+
+        var response = await call.ResponseAsync;
+        Console.WriteLine($"Final sum received from server: {response.Result}");
+    }
+
+    Console.WriteLine("Do you want to add more data for computation?.(0: No, 1: Yes)");
+    var answer = Console.ReadLine();
+    continueAdding = answer == "1";
+}
+
+
+
---
+++ GrpcServerClientStreaming/Services/CalculatorService.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Core;
+
+namespace GrpcServerClientStreaming.Services
+{
+    public class CalculatorService : Calculator.CalculatorBase
+    {
+        public override async Task<AddResponse> AddStream(IAsyncStreamReader<AddRequest> requestStream, ServerCallContext context)
+        {
+            uint sum = 0;
+
+            // when the stream ends, (client calls CompleteAsync) the MoveNext() will return false
+            while (await requestStream.MoveNext())
+            {
+                sum += requestStream.Current.Number;
+                Console.WriteLine($"Received number: {requestStream.Current.Number}, current sum: {sum}");
+            }
+
+            // return the final sum
+            Console.WriteLine($"Final sum: {sum}");
+            return new AddResponse { Result = sum };
+        }
+    }
+}
